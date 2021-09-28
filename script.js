15c6cb4fb71ad8d790a351225524a3218a0c8cd9
ysricky <yosuaricky2606@gmail.com>
2021-09-28 16:54:42 +0700

    add loading animation

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const btnNext = document.querySelector('.next');
 const inputText = document.querySelector('input');
 let inputKeyword;
 
+//loader
+const showSpinner = () => {
+  document.querySelector('.loader').style.display = 'block';
+};
+
+const hideSpinner = () => {
+  document.querySelector('.loader').style.display = 'none';
+};
+
 // promises
 async function getGif(keyword) {
   const image = document.querySelector('img');
-  console.log('log');
 
   try {
+    image.classList.add('hide');
+    showSpinner();
     const gifData = await fetch(
       `https://api.giphy.com/v1/gifs/translate?api_key=QG5BvJxlshVGggQvolh6RucaEXHw4cqQ&s=${keyword}`,
       { mode: 'cors' }
@@ -18,6 +28,9 @@ async function getGif(keyword) {
     image.src = gifImage.data.images.original.url;
   } catch (err) {
     console.error('Field is empty');
+  } finally {
+    hideSpinner();
+    image.classList.remove('hide');
   }
 }
 
@@ -30,6 +43,8 @@ btnSearch.addEventListener('click', () => {
 btnNext.addEventListener('click', () => {
   getGif(inputKeyword);
 });
+
+hideSpinner();
 
 //giphy api https://api.giphy.com/v1/gifs/translate?api_key=QG5BvJxlshVGggQvolh6RucaEXHw4cqQ&s=cats
 //openweather api http://api.openweathermap.org/data/2.5/weather?q=Jakarta&APPID=d0c59a3b8526fb27afb9aa24179956fb
